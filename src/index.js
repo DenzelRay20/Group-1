ea0e4a7ce1e0f571af477009b76a94406a3ff42b
JADEM <pagal.denzelray17@gmail.com>
2026-09-23 14:27:17 +0800

    Add PostgreSQL integration for students API git branch -M main git push -u origin main

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// A basic route
+// A basic route nyaaa
 app.get('/', (req, res) => {
   res.send('Hello, World! Express is working.');
 });
